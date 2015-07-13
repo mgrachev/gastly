d22258eef7cd754eb925ccf629aea2a997147033
Mikhail Grachev <work@mgrachev.com>
2015-07-13 16:27:15 +0300

    Add #resize and #format to Image

--- lib/gastly/image.rb
+++ lib/gastly/image.rb
@@ -10,5 +10,14 @@ module Gastly
       tempfile.unlink
     end
 
+    def resize(width:, height:)
+      dimensions = "#{width}x#{height}"
+      @file.resize(dimensions)
+    end
+
+    def format(ext)
+      @file.format(ext)
+    end
+
   end
 end
