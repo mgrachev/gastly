491e2768bdc39714eb19ac811f2b23a59cac5f6d
Mikhail Grachev <work@mgrachev.com>
2015-07-13 16:28:52 +0300

    Add default values to screenshot class

--- lib/gastly/screenshot.rb
+++ lib/gastly/screenshot.rb
@@ -1,5 +1,14 @@
+require 'phantomjs'
+
 module Gastly
   class Screenshot
+
+    SCRIPT_PATH = File.expand_path('../script.js', __FILE__)
+    DEFAULT_TIMEOUT = 0
+    DEFAULT_BROWSER_WIDTH = 1440
+    DEFAULT_BROWSER_HEIGHT = 900
+    DEFAULT_FILE_NAME = 'output'.freeze
+    DEFAULT_FILE_FORMAT = '.png'.freeze
 
   end
 end
