0c9ee8da853d9ec3c650569d98391d91d63eb1e9
Mikhail Grachev <work@mgrachev.com>
2015-07-13 16:26:43 +0300

    Add method Image#initialize

--- lib/gastly/image.rb
+++ lib/gastly/image.rb
@@ -1,5 +1,14 @@
+require 'mini_magick'
+
 module Gastly
   class Image
+
+    attr_reader :file
+
+    def initialize(tempfile)
+      @file = MiniMagick::Image.open(tempfile.path, 'png')
+      tempfile.unlink
+    end
 
   end
 end
