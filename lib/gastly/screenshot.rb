9e59ef0c07babf1e4b7bd91ed36bd0d27cc008f8
Mikhail Grachev <work@mgrachev.com>
2015-07-13 16:30:51 +0300

    Add method Screenshot#initalize

--- lib/gastly/screenshot.rb
+++ lib/gastly/screenshot.rb
@@ -1,4 +1,6 @@
 require 'phantomjs'
+require 'active_support/core_ext/hash/keys'
+require 'active_support/core_ext/object/blank'
 
 module Gastly
   class Screenshot
@@ -9,6 +11,18 @@ module Gastly
     DEFAULT_BROWSER_HEIGHT = 900
     DEFAULT_FILE_NAME = 'output'.freeze
     DEFAULT_FILE_FORMAT = '.png'.freeze
+
+    attr_writer :timeout, :browser_width, :browser_height
+    attr_accessor :url, :selector, :cookies
+
+    def initialize(url, **kwargs)
+      kwargs.assert_valid_keys(:timeout, :browser_width, :browser_height, :selector, :cookies)
+
+      @url = url
+      self.cookies = kwargs.delete(:cookies)
+
+      kwargs.each { |key, value| instance_variable_set(:"@#{key}", value) }
+    end
 
   end
 end
