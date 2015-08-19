0e29431d6cf54349a35e6fe0ef03c79c6134bf89
Mikhail Grachev <work@mgrachev.com>
2015-08-19 20:33:22 +0300

    Handle exceptions from phantom.js script

--- lib/gastly/screenshot.rb
+++ lib/gastly/screenshot.rb
@@ -5,8 +5,6 @@ require 'active_support/core_ext/object/blank'
 module Gastly
   class Screenshot
 
-    FetchError = Class.new(StandardError)
-
     SCRIPT_PATH = File.expand_path('../script.js', __FILE__)
     DEFAULT_TIMEOUT = 0
     DEFAULT_BROWSER_WIDTH = 1440
@@ -16,11 +14,13 @@ module Gastly
 
     attr_writer :timeout, :browser_width, :browser_height
     attr_accessor :url, :selector, :cookies, :proxy_host, :proxy_port
+    attr_reader :tempfile
 
     def initialize(url, **kwargs)
       kwargs.assert_valid_keys(:timeout, :browser_width, :browser_height, :selector, :cookies, :proxy_host, :proxy_port)
 
       @url = url
+      @tempfile = Tempfile.new([DEFAULT_FILE_NAME, DEFAULT_FILE_FORMAT])
       self.cookies = kwargs.delete(:cookies)
 
       kwargs.each { |key, value| instance_variable_set(:"@#{key}", value) }
@@ -39,25 +39,12 @@ module Gastly
     end
 
     def capture
-      tempfile = Tempfile.new([DEFAULT_FILE_NAME, DEFAULT_FILE_FORMAT])
-
-      params = {
-        url:      url,
-        timeout:  timeout,
-        width:    browser_width,
-        height:   browser_height,
-        output:   tempfile.path
-      }
-
-      params[:selector] = selector if selector.present?
-      params[:cookies]  = cookies.map { |k, v| "#{k}=#{v}" }.join(',') if cookies.present?
-      prepared_params   = params.map { |k, v| "#{k}=#{v}" }
-
       Phantomjs.proxy_host = proxy_host if proxy_host
       Phantomjs.proxy_port = proxy_port if proxy_port
+
       output = Phantomjs.run(proxy_options, SCRIPT_PATH.to_s, *prepared_params)
 
-      raise FetchError, output if output.present?
+      handle_exception(output) if output.present?
 
       Gastly::Image.new(tempfile)
     end
@@ -67,6 +54,34 @@ module Gastly
     def proxy_options
       return '' if proxy_host.blank? && proxy_port.blank?
       "--proxy=#{proxy_host}:#{proxy_port}"
+    end
+
+    def prepared_params
+      params = {
+        url:      url,
+        timeout:  timeout,
+        width:    browser_width,
+        height:   browser_height,
+        output:   tempfile.path
+      }
+
+      params[:selector] = selector if selector.present?
+      params[:cookies]  = hash_to_array(cookies).join(',') if cookies.present?
+
+      hash_to_array(params)
+    end
+
+    def hash_to_array(data)
+      data.map { |key, value| "#{key}=#{value}" }
+    end
+
+    def handle_exception(output)
+      error = case output
+              when /^FetchError:(.+)/     then Gastly::FetchError
+              when /^RuntimeError:(.+)/m  then Gastly::PhantomJSError
+              end
+
+      raise error, $1
     end
 
   end
